686bbf495f92d4a6ecd294b7a8a0989b45bd16b6
Hxttrick <andreasa.0404@gmail.com>
2025-07-31 17:52:04 +0200

    diverse skit

--- util.js
+++ util.js
@@ -1,1 +1,11 @@
 const clamp = (val, min, max) => Math.min(Math.max(val, min), max);
+
+async function tryCatch(promise) {
+    try {
+        const data = await promise;
+        return [data, null];
+    } catch (error) {
+        console.error(error);
+        return [null, error];
+    }
+}
